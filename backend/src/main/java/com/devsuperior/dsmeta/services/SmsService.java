cc52181843a0562e4d97d079277e4a1d86c2bb20
Wandreison Soares <wandreisonsoares@gmail.com>
2022-11-20 01:57:53 -0300

    Send notification

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,7 +34,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em" + date
+		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date
 				+ " com um total de R$ " + sale.getAmount();
 
 		Twilio.init(twilioSid, twilioKey);
